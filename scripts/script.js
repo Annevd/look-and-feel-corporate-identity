d28773e76eb5117151e897fe3139bbeeb08f11dd
Annevd <144004647+Annevd@users.noreply.github.com>
2023-11-20 13:06:12 +0100

    new code

---
+++ scripts/script.js
@@ -0,0 +1,47 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+// Zorgt ervoor dat je op de zoekbalk kan klikken zonder dat het menu weggaat
+
+document.getElementById("myDropdown").addEventListener("click",
+function(e) {
+
+e.stopPropagation();
+
+});
+
+  
+// Dark mode:
+
+const body = document.querySelector('body')
+toggle = body.querySelector(".toggle"),
+modeSwitch = body.querySelector(".toggle-switch"),
+modeText = body.querySelector(".mode-text");
+
+modeSwitch.addEventListener("click" , () =>{
+    body.classList.toggle("dark");
+    
+    if(body.classList.contains("dark")){
+        modeText.innerText = "Light mode";
+    }else{
+        modeText.innerText = "Dark mode";
+        
+    }
+});
